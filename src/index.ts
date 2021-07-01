e68bd590e3437639b918813e13198bbd1a7eee08
Harry G Withers <harrygwithers@gmail.com>
2021-07-01 08:42:09 +0100

    refactor: basic healthcheck server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { PORT = 3000 } = process.env
+
+const startServer = () => {
+    const app = express()
+
+    app.get('/healthcheck', (_, res) => res.status(200).send('OK'))
+
+    app.listen(PORT, () => {
+        console.log(`
+            ###########################################
+                   Server listening on port ${PORT}
+            ###########################################
+        `)
+    })
+}
+
+startServer()
