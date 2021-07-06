9b3163f7e5dca3d336ee15f5368aab3472894a6f
Harry G Withers <harrygwithers@gmail.com>
2021-07-06 08:17:58 +0100

    feat: build app with skaffold

---
+++ k8s/dev/create-cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dir=$(dirname ${BASH_SOURCE[0]})
+
+kind create cluster --config="$dir/cluster-config.yaml"
+
+VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/kind/deploy.yaml
+
+echo "Waiting for ingress to start ..."
+
+sleep 10
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+kind get clusters
