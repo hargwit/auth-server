67f68f317de2e371dfd4b2f95acdae9fc9fce3b7
Harry G Withers <harrygwithers@gmail.com>
2021-07-07 08:52:58 +0100

    fix: run cluster info after build cluster

--- k8s/dev/create-cluster.sh
+++ k8s/dev/create-cluster.sh
@@ -4,6 +4,8 @@ echo "Using cluster config at $1"
 
 kind create cluster --config=$1
 
+kubectl cluster-info --context kind-auth-example
+
 VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/kind/deploy.yaml
 
