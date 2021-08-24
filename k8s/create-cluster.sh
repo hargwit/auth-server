f4945ed59626179f1bab639f33392d357d818f4e
Harry G Withers <harrygwithers@gmail.com>
2021-08-24 08:18:45 +0100

    refactor: echo in create cluster script

--- k8s/create-cluster.sh
+++ k8s/create-cluster.sh
@@ -2,10 +2,13 @@
 
 echo "Using cluster config at $1"
 
+echo "Creating Kind cluster..."
+
 kind create cluster --config=$1
 
-echo "Cluster info"
-kubectl cluster-info --context kind-auth-example
+echo "Created Kind cluster"
+
+echo "Installing NGINX ingress..."
 
 VERSION=$(curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/stable.txt)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/${VERSION}/deploy/static/provider/kind/deploy.yaml
@@ -19,4 +22,10 @@ kubectl wait --namespace ingress-nginx \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
+echo "Installed NGINX ingress"
+
+echo "Installing cert-manager..."
+
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml
+
+echo "Installed cert-manager"
