367654273227bdb22029546f6ece05e094949f6a
Harry G Withers <harrygwithers@gmail.com>
2021-08-26 08:31:45 +0100

    test: signup

--- src/tests/example.int.test.ts
+++ src/tests/example.int.test.ts
@@ -1,18 +1,31 @@
 import axios from 'axios'
+import faker from 'faker'
 
 const { APP_URL = 'http://localhost' } = process.env
 
-test('should be alive', (done) => {
-    axios
+test('should be alive', async () => {
+    await axios
         .get(`${APP_URL}/healthcheck`)
         .then((res) => {
             expect(res.status).toBe(200)
-
-            done()
         })
         .catch((error) => {
             expect(error).toBeNull()
+        })
+})
 
-            done()
+test('should be able to sign up', async () => {
+    await axios
+        .post(`${APP_URL}/signup`, {
+            email: faker.internet.email(),
+            password: 'StrongPassword1@',
+        })
+        .then((res) => {
+            expect(res.status).toBe(200)
+
+            expect(res.headers['set-cookie']).not.toBeUndefined()
+        })
+        .catch((error) => {
+            expect(error).toBeNull()
         })
 })
